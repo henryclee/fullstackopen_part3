ebfb17c578f3f6deba67be554077bf0d902d45c5
Henry Lee <henrylee@buffalo.edu>
2023-01-01 09:46:48 -0500

    finished to 3.14

--- index.js
+++ index.js
@@ -1,61 +1,35 @@
+require ('dotenv').config
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
+const person = require('./models/person')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
-let persons = [
-    { 
-        "id": 1,
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-      },
-      { 
-        "id": 2,
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-      },
-      { 
-        "id": 3,
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-      },
-      { 
-        "id": 4,
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-      }
-]
-
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
-
-    if (person) {
+    
+    Person.findById(request.params.id).then(person => {
         response.json(person)
-    }
-    else {
-        response.statusMessage = "No such person"
-        response.status(404).end()
-    }
-
+    })
 })
 
 app.get('/info', (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people <br/>
-        ${Date()}`)
-})
 
-const generateID = () => {
-    
-    return (parseInt(Math.random() * 10000))
-}
+    Person.collection.countDocuments()
+        .then(num => {
+            response.send(`Phonebook has info for ${num} people <br/>
+            ${Date()}`)
+        })
+})
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -67,20 +41,23 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
+/*
     else if (persons.filter(p => p.name === body.name).length > 0) {
         return response.status(400).json({
             error: 'name must be unique'
         })
     }
+*/
 
-    const newPerson = {
-        id: generateID(),
+    const newPerson = new Person ({
         name: body.name,
         number: body.number || ""
-    }
+    })
 
-    persons = persons.concat(newPerson)
-    response.json(persons)
+    newPerson.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+
 })
 
 app.delete('/api/persons/:id', (request, response) =>{
