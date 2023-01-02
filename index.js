fdea8e4a000059d4bbff388c5e3af4f044e16aca
Henry Lee <henrylee@buffalo.edu>
2023-01-02 12:29:04 -0500

    finished to 3.18, works locally only

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require ('dotenv').config
 const express = require('express')
 const app = express()
-const cors = require('cors')
+//const cors = require('cors')
 const Person = require('./models/person')
 const person = require('./models/person')
 
-app.use(cors())
+//app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
@@ -31,7 +31,7 @@ app.get('/info', (request, response) => {
         })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
 
@@ -40,14 +40,6 @@ app.post('/api/persons', (request, response) => {
             error: 'missing information'
         })
     }
-
-/*
-    else if (persons.filter(p => p.name === body.name).length > 0) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-*/
 
     const newPerson = new Person ({
         name: body.name,
@@ -60,12 +52,45 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-app.delete('/api/persons/:id', (request, response) =>{
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response,next) =>{
+
+    Person.findByIdAndRemove(request.params.id)
+        .then (result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    const update = {
+        name: body.name,
+        number: body.number
+    }
+    Person.findByIdAndUpdate(request.params.id, update, {new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+
+// Error Handling middleware
+
+const unknownEndpoint = (request,response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name = 'CastError') {
+        return response.status(400).send({error: 'malformed id'})
+    }
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
